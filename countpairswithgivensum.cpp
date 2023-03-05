3c690203cf292b8d6443079baca4885fe8b663ae
Sanidhya Jain <sanidhyajain45@gmail.com>
2023-03-05 20:12:18 +0530

    Update countpairswithgivensum.cpp

--- countpairswithgivensum.cpp
+++ countpairswithgivensum.cpp
@@ -16,23 +16,51 @@ public:
         //     }
         // }
         // return count;
-        sort(arr, arr+n);
-        map<int, int> mp;
+        // sort(arr, arr+n);
+        // map<int, int> mp;
         
-        for(int i=0;i<n;i++)
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(mp.find(arr[i])!=mp.end())
+        //         mp[arr[i]]+=1;
+        //     else mp[arr[i]]=1;
+        // }
+        // int count=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     int val= k-arr[i];
+        //     count+=mp[val];
+        //     if(val== arr[i])
+        //         count--;
+        // }
+        // return count/2;
+        // two pointer approach below
+        int i=0, j=n-1, ans=0;
+        sort(arr, arr +n);
+        while(i<j)
         {
-            if(mp.find(arr[i])!=mp.end())
-                mp[arr[i]]+=1;
-            else mp[arr[i]]=1;
+            if(arr[i] + arr[j] < k)
+                i++;
+            else if(arr[i] + arr[j] >k)
+                j--;
+            else{
+                
+                int x=arr[i], xx=i;
+                while(i<j && arr[i] ==x)
+                i++;
+                
+                int y=arr[j], yy=j;
+                while(j>=i && arr[j] ==y)
+                j--;
+                
+                if(x==y)
+                    {int temp= i-xx + yy-j -1;
+                    ans= ans + (temp*(temp+1))/2;}
+                else{
+                    ans= ans+ (i-xx) * (yy-j);
+                }
+            }
         }
-        int count=0;
-        for(int i=0;i<n;i++)
-        {
-            int val= k-arr[i];
-            count+=mp[val];
-            if(val== arr[i])
-                count--;
-        }
-        return count/2;
+        return ans;
     }
 };
