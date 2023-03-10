e4cf0a474ad84d6b567157c7cba627b04748a579
Sanidhya Jain <sanidhyajain45@gmail.com>
2023-03-10 18:54:01 +0530

    Update tree_heightofbt.cpp

--- tree_heightofbt.cpp
+++ tree_heightofbt.cpp
@@ -13,6 +13,20 @@ struct Node
     }
 };
 */
+// another way (using only recursion)
+ int height(struct Node* node){
+        // code here 
+        if(node==NULL) return 0;
+        if (node->left==NULL && node->right==NULL)
+            return 1;
+        if(node->left==NULL && node->right!=NULL)
+            return 1+ height(node->right);
+        if(node->left!=NULL && node->right==NULL)
+            return 1+ height(node->left);
+        else 
+            return 1 + max( height(node->left), height(node->right) );
+ }
+// another way
 class Solution{
     public:
     int k=INT_MIN;
