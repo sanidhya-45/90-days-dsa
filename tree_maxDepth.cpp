25afe998a014231123fa03d769efada16b5fe36a
Sanidhya Jain <sanidhyajain45@gmail.com>
2023-04-13 17:08:17 +0530

    Add files via upload

---
+++ tree_maxDepth.cpp
@@ -0,0 +1,9 @@
+class Solution{
+  public:
+    /*You are required to complete this method*/
+    int maxDepth(Node *root) {
+        // Your code here
+        if(root==NULL) return 0;
+        return 1 + max( maxDepth(root->left), maxDepth(root->right) );
+    }
+};
